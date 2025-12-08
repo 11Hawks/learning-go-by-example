487a4b71c93d918d2b3263185c2965a57519624a
Hawks11 <197453050+11Hawks@users.noreply.github.com>
2025-12-08 01:36:21 -0500

    corrected typos in internal/main.go file

--- go-rest-api/internal/main.go
+++ go-rest-api/internal/main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
 	"github.com/go-openapi/loads"
-    "github.com/go-openapi/runtime/middleware"
-    "github.com/11Hawks/learning-go-by-example/go-rest-api/pkg/swagger/server/restapi"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/scraly/learning-go-by-examples/go-rest-api/pkg/swagger/server/restapi"
 
-    "github.com/11Hawks/learning-go-by-example/go-rest-api/pkg/swagger/server/restapi/operations"
+	"github.com/scraly/learning-go-by-examples/go-rest-api/pkg/swagger/server/restapi/operations"
 )
+
 func main() {
 	//Initialize Swagger
 	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
 	if err != nil {
-		log.Fatlln(err)
+		log.Fatalln(err)
 	}
 
 	api := operations.NewHelloAPIAPI(swaggerSpec)
@@ -34,39 +34,40 @@ func main() {
 	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
 
 	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
-	
+
 	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
 
 	//Start server which listening
-	if err: = server.Serve(); err != nil {
+	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
 	}
 }
-//Health route returns OK
+
+// Health route returns OK
 func Health(operations.CheckHealthParams) middleware.Responder {
-    return operations.NewCheckHealthOK().WithPayload("OK")
+	return operations.NewCheckHealthOK().WithPayload("OK")
 }
 
-//GetHelloUser returns Hello + your name
+// GetHelloUser returns Hello + your name
 func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
-    return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
 }
 
-//GetGopherByName returns a gopher in png
+// GetGopherByName returns a gopher in png
 func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
 
-    var URL string
-    if gopher.Name != "" {
-        URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
-    } else {
-        //by default we return dr who gopher
-        URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
-    }
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		//by default we return dr who gopher
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+	}
 
-    response, err := http.Get(URL)
-    if err != nil {
-        fmt.Println("error")
-    }
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("error")
+	}
 
-    return operations.NewGetGopherNameOK().WithPayload(response.Body)
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
 }
